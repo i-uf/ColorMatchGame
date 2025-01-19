380515ded02931fe23ed5ff894518361cf1d9026
chodcha <uf_developer@outloo.kr>
2025-01-19 21:01:44 +0900

    i

--- src/main/java/com/i_uf/Game.java
+++ src/main/java/com/i_uf/Game.java
@@ -5,7 +5,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseWheelEvent;
 import java.io.IOException;
 import java.util.List;
 import java.util.*;
